3dffd5d197acc6f8f73f852dc8b09e901f8ca2ae
Joseph Bane <jbane@acm.org>
2023-12-08 13:34:59 -0500

    Added comment to 8-2

--- 8-2.py
+++ 8-2.py
@@ -26,6 +26,7 @@ with open(filename, 'r') as f:
 # Find the distances from starting nodes to end nodes; the least common multiple of all those distances is the answer.
 # This is because it would be the smallest number of instruction cycles to have all nodes reach the end at the same time.
 # I *think* this works because all our test data has paths from Z back to that same Z that are the same distance from their A to Z.
+# Edit: actually it's becase the values for each cycle are exact multiples of the instruction's length!
 distances = []
 curr_nodes = [node for node in nodes.keys() if node[-1] == 'A']
 for curr_node in curr_nodes:
