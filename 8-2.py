f5b28b4da27502e65cb1f7377601746548e26fb4
Joseph Bane <jbane@acm.org>
2023-12-08 13:28:41 -0500

    Cleanup 8-2 solution

--- 8-2.py
+++ 8-2.py
@@ -23,20 +23,20 @@ with open(filename, 'r') as f:
                 'R': right.strip(),
             }
 
-# Find the distances from starting nodes to end nodes and include cycle distances from end nodes;
-# the least common multiple of all those distances is the answer because it would be the smallest
-# number of cycles to have all nodes reach the end at the same time:
-distances = {}
-curr_nodes = [node for node in nodes.keys() if node[-1] in ('A', 'Z')]
+# Find the distances from starting nodes to end nodes; the least common multiple of all those distances is the answer.
+# This is because it would be the smallest number of instruction cycles to have all nodes reach the end at the same time.
+# I *think* this works because all our test data has paths from Z back to that same Z that are the same distance from their A to Z.
+distances = []
+curr_nodes = [node for node in nodes.keys() if node[-1] == 'A']
 for curr_node in curr_nodes:
     steps = 0
     instruction = 0
-    while curr_node[-1] != 'Z' or steps == 0:
+    while curr_node[-1] != 'Z': # or steps == 0:
         l_or_r = instructions[instruction]
         curr_node = nodes[curr_node][l_or_r]
         steps += 1
-        instruction = (instruction + 1) % len(instructions) # account for looping
-    distances[curr_node] = steps
+        instruction = (instruction + 1) % len(instructions) # account for instruction looping to get to our Z
+    distances.append(steps)
 
 pprint(distances)
-print(math.lcm(*distances.values()))
+print(math.lcm(*distances))
