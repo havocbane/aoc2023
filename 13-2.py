429273d8d5c307ed84c9bb3f37f8f71e257e7c9e
Joseph Bane <jbane@acm.org>
2023-12-13 20:16:56 -0500

    Advent of Code 2023: Day 13 (part 2)

---
+++ 13-2.py
@@ -0,0 +1,53 @@
+from collections import Counter
+
+# filename = '13.test.txt'
+filename = '13.txt'
+
+grids = []
+
+with open(filename, 'r') as f:
+    grid = []
+    for line in f:
+        if not line.strip():
+            grids.append(grid)
+            grid = []
+            continue
+        grid.append(list(line.strip()))
+    grids.append(grid)
+
+def diff_lines(left, right):
+    diff_count = 0
+    diff_loc = 0
+    for x in range(len(left)):
+        if left[x] != right[x]:
+            diff_count += 1
+            diff_loc = x
+    return diff_count, diff_loc
+
+def find_reflection(grid):
+    for x in range(len(grid) - 1):
+        left = grid[:x + 1]
+        left.reverse()
+        right = grid[x + 1:]
+
+        max_c = min(len(left), len(right))
+        diff_counts = Counter(
+            str(diff_lines(left[y], right[y])[0])
+            for y in range(max_c)
+        )
+        if diff_counts["1"] == 1 and set(diff_counts.keys()) - {"0", "1"} == set():
+            assert diff_counts["0"] + diff_counts["1"] == max_c, diff_counts
+            return x + 1
+    return -1
+
+r_ct = c_ct = 0
+for grid in grids:
+    rows_before = find_reflection(grid)
+    if rows_before != -1:
+        r_ct += rows_before
+    else:
+        columns = list(zip(*grid))
+        cols_before = find_reflection(columns)
+        if cols_before != -1:
+            c_ct += cols_before
+print(c_ct + 100 * r_ct)
